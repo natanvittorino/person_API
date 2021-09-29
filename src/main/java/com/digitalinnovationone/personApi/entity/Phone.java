7a1e9ed60b3f143aaed9bede4e5c87feecbc554d
natanvittorino <natanvittorino2020@gmail.com>
2021-09-28 23:21:58 -0300

    Phone class was annotated with Entity. Primary key and generation type were created. Fields number and type were made not null. Field number was annotated with Enumerated(EnumType.STRING

--- src/main/java/com/digitalinnovationone/personApi/entity/Phone.java
+++ src/main/java/com/digitalinnovationone/personApi/entity/Phone.java
@@ -6,13 +6,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private String number;
+
+    @Column(nullable = false)
     private PhoneType type;
 }
