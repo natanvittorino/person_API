7454233409b54e34c413a50e3bd615ded342e2e4
natanvittorino <natanvittorino2020@gmail.com>
2021-09-28 23:05:41 -0300

    Annotations and fields were added to Person and Phone

--- src/main/java/com/digitalinnovationone/personApi/entity/Phone.java
+++ src/main/java/com/digitalinnovationone/personApi/entity/Phone.java
@@ -1,4 +1,18 @@
 package com.digitalinnovationone.personApi.entity;
 
+import com.digitalinnovationone.personApi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Phone {
+
+    private Long id;
+    private String number;
+    private PhoneType type;
 }
