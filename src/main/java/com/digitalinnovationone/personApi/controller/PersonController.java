df09629c0dbcae1e3420fe127254026a531ae615
natanvittorino <natanvittorino2020@gmail.com>
2021-09-30 23:37:46 -0300

    PersonController: PersonService was injected into it; CreatedPerson method was implemented.

--- src/main/java/com/digitalinnovationone/personApi/controller/PersonController.java
+++ src/main/java/com/digitalinnovationone/personApi/controller/PersonController.java
@@ -2,30 +2,25 @@ package com.digitalinnovationone.personApi.controller;
 
 import com.digitalinnovationone.personApi.dto.response.MessageResponseDTO;
 import com.digitalinnovationone.personApi.entity.Person;
-import com.digitalinnovationone.personApi.repository.PersonRepository;
+import com.digitalinnovationone.personApi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
-
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person) {
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with Id " +  savedPerson.getId())
-                .build();
+        return personService.createPerson(person);
     }
 }
